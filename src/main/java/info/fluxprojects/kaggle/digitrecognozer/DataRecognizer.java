b7ce8e27b4914c40466943fa1cc3c16e3f33e6bb
Jeroen Meulemeester <jeroen.meulemeester@gmail.com>
2014-11-01 13:51:00 +0100

    Quick test class to get some statistics

---
+++ src/main/java/info/fluxprojects/kaggle/digitrecognozer/DataRecognizer.java
@@ -0,0 +1,37 @@
+package info.fluxprojects.kaggle.digitrecognozer;
+
+import info.fluxprojects.kaggle.digitrecognozer.files.TrainingDataEntry;
+
+import java.util.IntSummaryStatistics;
+import java.util.List;
+
+/**
+ * Date: 28/10/14 - 21:05
+ *
+ * @author Jeroen Meulemeester
+ */
+public class DataRecognizer {
+
+	/**
+	 * The training data resource
+	 */
+	public static final String TRAINING_DATA_FILE_NAME = DataRecognizer.class.getResource("/train.csv").getFile();
+
+	public static void main(String[] args) throws Exception {
+		final List<TrainingDataEntry> entries = TrainingDataEntry.fromFile(TRAINING_DATA_FILE_NAME);
+		final IntSummaryStatistics[] statistics = new IntSummaryStatistics[10];
+		for (int i = 0; i < statistics.length; i++) {
+			final int index = i;
+			statistics[i] = entries.parallelStream()
+					.filter(e -> e.getSolution() == index)
+					.mapToInt(e -> e.countPixels(p -> p > 0))
+					.summaryStatistics();
+		}
+
+		for (int i = 0; i < statistics.length; i++) {
+			System.out.println(i + "  ->  " + statistics[i]);
+		}
+
+	}
+
+}
